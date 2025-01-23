5575271f0d661655543840f80e32e89bbc91ee2a
Leonty <krokxa228@gmail.com>
2025-01-23 20:36:35 +0500

    modified sidemenu

--- React/Diagnostics-app/src/components/Pages/SideMenu.tsx
+++ React/Diagnostics-app/src/components/Pages/SideMenu.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Logout } from "./Navbar";
 import "../CSS/SideMenu.css";
 import { useSidebar } from "../../DataContext/SidebarContext";
@@ -15,6 +15,7 @@ const Sidebar: React.FC<SidebarProps> = ({ login }) => {
   const navigate = useNavigate();
   const { isSidebarOpen } = useSidebar();
   const { openSections, toggleSection } = useSidebarContext(); // Используем контекст
+
   const sectionList = [
     { path: "network", label: "Сеть" },
     { path: "accidents", label: "Аварии" },
@@ -23,23 +24,12 @@ const Sidebar: React.FC<SidebarProps> = ({ login }) => {
     { path: "app", label: "Приложение" },
   ];
 
-  useEffect(() => {
-    // Инициализируем состояние открытых секций при загрузке
-    if (Object.keys(openSections).length === 0) {
-      jsonData.forEach((_, index) => toggleSection(index));
-    }
-  }, [jsonData, toggleSection]);
-
   // Парсим query-параметры
   const query = new URLSearchParams(location.search);
   const selectedSubsectionName = query.get("subsection");
-  const queriedLogin = query.get("login");
 
   const openSubsection = (subsectionName: string) => {
-    let url = `/subsection?subsection=${encodeURIComponent(subsectionName)}`;
-    if (queriedLogin) {
-      url += `&login=${encodeURIComponent(queriedLogin)}`;
-    }
+    const url = `/subsection?subsection=${encodeURIComponent(subsectionName)}&login=${encodeURIComponent(login)}`;
     navigate(url);
   };
 
@@ -47,81 +37,60 @@ const Sidebar: React.FC<SidebarProps> = ({ login }) => {
     <div className="d-flex">
       <div className={`side-menu-wrapper ${isSidebarOpen ? "open" : ""}`}>
         <div className="side-menu">
-          {jsonData.map((section, index) => (
-            <div key={index} className="section">
-              <h3
-                className="section-title"
-                onClick={() => toggleSection(index)}
-              >
-                {section.section}
-                <span
-                  className={`toggle-icon ${openSections[index] ? "open" : ""}`}
-                >
-                  {openSections[index] ? "▲" : "▼"}
-                </span>
-              </h3>
+          {jsonData.map((section, index) => {
+            const isMatchedSection = sectionList.find(
+              (item) => item.label === section.section
+            );
+            
+            // Проверяем, если секция имеет ссылку или нет
+            const hasLink = isMatchedSection && isMatchedSection.path;
 
-              <div
-                className={`subsections ${
-                  openSections[index] ? "open" : "closed"
-                }`}
-              >
-                {sectionList.some((item) => item.label === section.section) && (
-                  <div
-                    className={`subsection ${
-                      login &&
-                      sectionList.some(
-                        (item) =>
-                          item.label === section.section &&
-                          location.pathname.includes(item.path)
-                      )
-                        ? "active"
-                        : ""
-                    }`}
-                  >
-                    {sectionList.map((item) =>
-                      item.label === section.section ? (
-                        <div
-                          key={item.path}
-                          onClick={() => {
-                            if (login) {
-                              navigate(
-                                `/${item.path}?login=${encodeURIComponent(
-                                  login
-                                )}`
-                              );
-                            }
-                          }}
-                          className={`subsection-content ${
-                            !login ? "disabled" : ""
-                          }`}
-                          title={!login ? "Введите логин" : ""}
-                        >
-                          Основной
-                        </div>
-                      ) : null
-                    )}
-                  </div>
-                )}
-                {section.subsections &&
-                  section.subsections.map((subsection, subIndex) => (
-                    <div
-                      key={subIndex}
-                      className={`subsection ${
-                        subsection.subsection === selectedSubsectionName
-                          ? "active"
-                          : ""
-                      }`}
-                      onClick={() => openSubsection(subsection.subsection)}
-                    >
-                      <div className="subsection-content">
-                        {subsection.subsection}
+            // Если секция имеет ссылку, то проверяем активность на основе текущего пути
+            const isActiveSection = hasLink
+              ? location.pathname.includes(isMatchedSection.path) // Если есть путь, то проверяем его
+              : false;
+
+            return (
+              <div key={index} className="section">
+                <h3
+                  className={`section-title ${isActiveSection ? "active" : ""}`}
+                  onClick={() => {
+                    if (openSections[index]) {
+                      // Если секция открыта
+                      toggleSection(index);
+                    } else {
+                      // Если секция закрыта
+                      toggleSection(index);
+                      if (hasLink) {
+                        // Если секция привязана к ссылке, переходим по ней
+                        navigate(`/${isMatchedSection.path}?login=${encodeURIComponent(login)}`);
+                      }
+                    }
+                  }}
+                >
+                  {section.section}
+                  <span className={`toggle-icon ${openSections[index] ? "open" : ""}`}>
+                    {openSections[index] ? "▲" : "▼"}
+                  </span>
+                </h3>
+
+                <div className={`subsections ${openSections[index] ? "open" : "closed"}`}>
+                  {section.subsections &&
+                    section.subsections.map((subsection, subIndex) => (
+                      <div
+                        key={subIndex}
+                        className={`subsection ${
+                          subsection.subsection === selectedSubsectionName ? "active" : ""
+                        }`}
+                        onClick={() => openSubsection(subsection.subsection)}
+                      >
+                        <div className="subsection-content">{subsection.subsection}</div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
           <div className="mt-auto">
             <Logout />
           </div>
